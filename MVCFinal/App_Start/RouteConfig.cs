d32d6d58d90a1177651f925b5ffb1f7cc9b7de8e
MatiasMelfi <matiasnicolasmelfi@gmail.com>
2016-10-31 22:09:56 -0300

    esta la estructura de todas las master echas
    ya arme la estructura de todas las master y defini todas las view que va
    a tener el sistema a mi entender me falta hacer el diseño de las pages
    en especifico entre mañana y pasado lo hago

--- MVCFinal/App_Start/RouteConfig.cs
+++ MVCFinal/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCFinal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Pais", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "LoginUsuario", id = UrlParameter.Optional }
             );
         }
     }
