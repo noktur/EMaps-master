bc1037d216f12892d1a4ddbd3ab7e30313f5de10
RuberBoy <el_negro_ger@hotmail.com>
2016-11-24 14:44:47 -0300

    AltaFotoLugar, AgregoFotosLugar

--- Base de datos.sql
+++ Base de datos.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS SistemaEventos ;
+﻿DROP DATABASE IF EXISTS SistemaEventos ;
 
 CREATE database IF NOT EXISTS SistemaEventos ;
 
@@ -878,6 +878,19 @@ DELIMITER //
 CREATE PROCEDURE AltaFotosLugar (pNombreFoto varchar(30),pFoto varbinary(300),pNombreLugar varchar(30))
 BEGIN
 INSERT INTO FotosLugar(NombreFoto,Foto,NombreLugar) VALUES(pNombreFoto,pFoto,pNombreLugar);
+END//
+
+DELIMITER //
+CREATE PROCEDURE EliminarFotosLugar (pNombreFoto varchar(30),pNombreLugar varchar(30))
+BEGIN
+Delete from FotosLugar
+where NombreFoto = pNombreFoto and NombreLugar = pNombreLugar;
+END//
+
+CREATE PROCEDURE ListarFotosLugar (pNombreLugar varchar(30))
+BEGIN
+Select * from FotosLugar
+where NombreLugar = pNombreLugar;
 END//
 
 -- PROCEDIMIENTOS ALMACENADOS AREAS
---
+++ Persistencia/Interfaces/IPersistenciaFotosLugar.cs
@@ -0,0 +1,14 @@
+﻿using EntidadesCompartidas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistencia.Interfaces
+{
+    public interface IPersistenciaFotosLugar
+    {
+        void AltaFotoLugar(FotosLugar F, Lugar L);
+        void AgregarFotosLugar(Lugar L);
+    }
+}
